a8ff594c80691e8587410f06e88deecde57e41eb
Duc <tuanduc.tran@outlook.com>
2025-11-18 08:02:04 +0700

    Remove comment

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World main2!", app.getMessage());
+        assertEquals("Hello World main3!", app.getMessage());
     }
 }
