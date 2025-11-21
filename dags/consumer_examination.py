dbdef7f23f2910190dfb39bbc5a247630a3eb8a9
Duc <tuanduc.tran@outlook.com>
2025-11-21 16:39:08 +0700

    Fix syntax error

--- dags/consumer_examination.py
+++ dags/consumer_examination.py
@@ -10,14 +10,14 @@ default_args = {
 }
 
 with DAG(
-    dag_id="first dag",
+    dag_id="first_dag",
     description="My first dag",
     start_date=datetime(2025, 11, 20),
     schedule_interval="@daily",
 ) as dag:
     pass
     task1 = BashOperator(
-        task_id="first task",
+        task_id="first_task",
         bash_command="echo Helloworld, this is my first dag!",
     )
 
